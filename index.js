ca94bcf515f443881238c5aded83902e26513688
masumwbdev <masumwbd@gmail.com>
2022-03-23 17:38:29 +0600

    created post api

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 5000
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qr0wp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("doctPortal");
+      const appoinmentCollection = database.collection("appoinment");
+      
+        // appoinment post
+        app.post('/appoinment', async(req, res) => {
+            const appoinment = req.body;
+            const result = await appoinmentCollection.insertOne(appoinment)
+            // console.log(appoinment);
+            res.json(result)
+        })
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
